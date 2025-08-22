c330e9b54d39ea72b1f96fa275b5add61ca71b45
Matheussokolovski <matheussokolovski@gmail.com>
2025-08-21 22:24:58 -0300

    projeto criado

---
+++ projetojavagit/src/main/java/com/sokolovski/projetojavagit/ProjetojavagitApplication.java
@@ -0,0 +1,14 @@
+package com.sokolovski.projetojavagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojavagitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojavagitApplication.class, args);
+	}
+
+
+}
